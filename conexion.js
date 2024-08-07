b501ede5092d834d1eaa7de9560f142de3ee0f0d
testDeploy <qhse.auxiliar.mx@r-pac.com>
2024-08-07 11:48:36 -0600

    corrección conexión a la db

--- conexion.js
+++ conexion.js
@@ -2,33 +2,32 @@ import {Sequelize} from "sequelize";
 import dotenv from "dotenv"
 dotenv.config();
 
-//conexion a la base de datos (investigar como se conecta a una db mysql en servidor)
-const sequelize = new Sequelize(process.env.DATA_BASE_URL || 
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        /*dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            }
-        },*/
-        pool: {
-            max: 10,
-            min: 0,
-            idle: 60000
-        },
-        define: {
-            freezeTableName: true,
-            timestamps: false,
-            defaultScope: {
-                attributes: { exclude: ["createdAt", "updateAt" ] }
-               }
-            }
+const options = {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    /*dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        }
+    },*/
+    pool: {
+        max: 10,
+        min: 0,
+        idle: 60000
+    },
+    define: {
+        freezeTableName: true,
+        timestamps: false,
+        defaultScope: {
+            attributes: { exclude: ["createdAt", "updateAt" ] }
+           }
     }
-);
+}
+
+//conexion a la base de datos (investigar como se conecta a una db mysql en servidor)
+const sequelize = process.env.DATA_BASE_URL 
+    ? new Sequelize(process.env.DATA_BASE_URL, options ) 
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, options)
 
 export default sequelize;
